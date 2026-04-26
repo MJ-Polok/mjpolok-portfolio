150eda4c302347a38f6f3346ad23c9c34d6a595f
MJ Polok <mjpolok018@gmail.com>
2026-04-26 14:31:38 +0600

    perf: optimize aurora animation for mobile

--- src/components/AuroraBackground.tsx
+++ src/components/AuroraBackground.tsx
@@ -1,4 +1,3 @@
-
 import { cn } from "../lib/utils";
 import React from "react";
 import type { ReactNode } from "react";
@@ -43,31 +42,31 @@ export const AuroraBackground = ({
                     } as React.CSSProperties
                 }
             >
-                {/* Overlay */}
                 <div className="absolute inset-0 bg-slate-950/35 backdrop-blur-[2px]" />
 
-                {/* Animated Aurora */}
                 <div
                     className={cn(
-                        "pointer-events-none absolute -inset-[10px] opacity-50 blur-[10px] invert dark:invert-0 filter will-change-transform animate-aurora",
+                        "pointer-events-none absolute -inset-[10px] filter will-change-transform animate-aurora",
+                        "opacity-30 blur-[6px]",
+                        "md:opacity-50 md:blur-[10px]",
+                        "invert dark:invert-0",
                         "[background-image:var(--dark-gradient),var(--aurora)]",
                         "[background-size:300%,_200%]",
                         "[background-position:50%_50%,50%_50%]",
                         "[--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]",
                         "[--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]",
                         "[--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]",
-                        "after:absolute after:inset-0 after:content-['']",
-                        "after:[background-image:var(--dark-gradient),var(--aurora)]",
-                        "after:[background-size:200%,_100%]",
-                        "after:[background-attachment:fixed]",
-                        "after:mix-blend-difference",
+                        "md:after:absolute md:after:inset-0 md:after:content-['']",
+                        "md:after:[background-image:var(--dark-gradient),var(--aurora)]",
+                        "md:after:[background-size:200%,_100%]",
+                        "md:after:[background-attachment:fixed]",
+                        "md:after:mix-blend-difference",
                         showRadialGradient &&
                         "[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]",
                     )}
                 />
             </div>
 
-            {/* Content */}
             <div className="relative z-10 w-full">{children}</div>
         </div>
     );
